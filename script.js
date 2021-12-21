d4e2fb9aa32d08b70ef89add89224376fdd4beaf
Raul <raul.santos.calvo@gmail.com>
2021-12-21 10:57:59 +0000

    add focus to input after alert

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ function compute()
         + "at an interest rate of \<span class='highlight'\>" + rate + "%\<\/span\>.\<br\>"
         + "You will receive an amount of \<span class='highlight'\>" + interest + "\<\/span\>,\<br\>"
         + "in the year \<span class='highlight'\>" + year + "\<\/span\>."
-        // returns focus to principal input
-        document.getElementById("principal").focus();
     } else {
         window.alert('Enter a positive number');
     }
+    // returns focus to principal input
+    document.getElementById("principal").focus();
 }
 
 /**
